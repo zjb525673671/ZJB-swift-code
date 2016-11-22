e55777b0b9131c0f0489e107e68823e9acb96caf
zhoujiebing436@pingan.com.cn <zhoujiebing436@pingan.com>
2016-11-22 14:34:21 +0800

    测试我的代码

---
+++ ZJBTestOne/ZJBTestOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZJBTestOne
+//
+//  Created by 周结兵 on 2016/11/22.
+//  Copyright © 2016年 zjb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
