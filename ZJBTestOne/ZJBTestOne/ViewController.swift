76a1d546ff2d8e4226ad1f4b48203f4f3b84f191
zhoujiebing436@pingan.com.cn <zhoujiebing436@pingan.com>
2016-11-22 14:38:35 +0800

    做我的代码

--- ZJBTestOne/ZJBTestOne/ViewController.swift
+++ ZJBTestOne/ZJBTestOne/ViewController.swift
@@ -15,9 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
     }
 
 
