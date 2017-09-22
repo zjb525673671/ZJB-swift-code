e85ba0b257f007b984b8b2f95465003b98c8c681
周结兵 <zjb525673671@126.com>
2017-09-22 15:08:50 +0800

    增加下拉刷新三方库

--- ZJBTestOne/Pods/Target Support Files/Pods-ZJBTestOne/Pods-ZJBTestOne-frameworks.sh
+++ ZJBTestOne/Pods/Target Support Files/Pods-ZJBTestOne/Pods-ZJBTestOne-frameworks.sh
@@ -90,6 +90,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/DGElasticPullToRefresh/DGElasticPullToRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/IBAnimatable/IBAnimatable.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Moya/Moya.framework"
@@ -100,6 +101,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/DGElasticPullToRefresh/DGElasticPullToRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/IBAnimatable/IBAnimatable.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Moya/Moya.framework"
--- ZJBTestOne/ZJBTestOne/Function/ActivePage/Controller/ActivityMainViewController.swift
+++ ZJBTestOne/ZJBTestOne/Function/ActivePage/Controller/ActivityMainViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import DGElasticPullToRefresh
 
 class ActivityMainViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
 
